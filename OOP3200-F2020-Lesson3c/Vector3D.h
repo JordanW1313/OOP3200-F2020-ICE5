e21814f8bd0326d4f0512ea4b34174d834c87d02
JordanW1313 <jwriker@gmail.com>
2020-10-14 11:56:27 -0400

    updated vector 3D

--- OOP3200-F2020-Lesson3c/Vector3D.h
+++ OOP3200-F2020-Lesson3c/Vector3D.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
+#include <iostream>
+#include "Vector2D.h"
+#include "assert.h"
 #ifndef __VECTOR3D__
 #define __VECTOR3D__
 
@@ -8,6 +11,8 @@ class Vector3D
 {
 public:
 	Vector3D(const T x = static_cast<T>(0.0), const T y = static_cast<T>(0.0), const T z = static_cast<T>(0.0));
+	Vector3D(const std::string& x, const std::string& y, const std::string& z);
+	Vector3D(const Vector2D& vector2d);
 	~Vector3D();
 
 	//accessors
@@ -21,6 +26,23 @@ public:
 	void SetZ(const T z);
 	void Set(const T x, const T y, const T z);
 
+	friend std::ostream& operator<<(std::ostream& out, const Vector3D& rhs)
+	{
+		out << rhs.ToString();
+		return out;
+	}
+
+	friend std::istream& operator>>(std::istream& in, Vector3D& rhs)
+	{
+		in >> rhs.m_x;
+		in.ignore();
+		in >> rhs.m_y;
+		in.ignore();
+		in >> rhs.m_z;
+
+		return in;
+	}
+
 	std::string ToString() const;
 
 private:
@@ -32,7 +54,42 @@ private:
 /* Implementation Section */
 template <class T>
 Vector3D<T>::Vector3D(const T x, const T y, const T z) : m_x(x), m_y(y), m_z(z)
+{}
+
+template<class T>
+Vector3D<T>::Vector3D(const std::string& x, const std::string& y, const std::string& z)
 {
+	if (typeid(T) == typeid(int))
+	{
+		m_x = std::stoi(x);
+		m_y = std::stoi(y);
+		m_z = std::stoi(z);
+	}
+	
+	if (typeid(T) == typeid(float))
+	{
+		m_x = std::stof(x);
+		m_y = std::stof(y);
+		m_z = std::stof(z);
+	}
+
+	if (typeid(T) == typeid(double))
+	{
+		m_x = std::stod(x);
+		m_y = std::stod(y);
+		m_z = std::stod(z);
+	}
+
+	assert(typeid(T) != typeid(int) || typeid(T) != typeid(float) || typeid(T) != typeid(double)),
+		"error no conversion possible";
+}
+
+template<class T>
+Vector3D<T>::Vector3D(const Vector2D& vector2d)
+{
+	m_x = static_cast<T>(vector2d.GetX());
+	m_y = static_cast<T>(vector2d.GetY());
+	m_z = static_cast<T>(0.0);
 }
 
 template <class T>
